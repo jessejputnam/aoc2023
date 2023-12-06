8aab737eb51778c0ceb6459de2446b77af70389a
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2023-12-06 11:32:43 -0500

    Update fetch to handle cases with trailing new line char

--- lib/func.py
+++ lib/func.py
@@ -6,7 +6,10 @@ def fetch(day):
     url = f"https://adventofcode.com/2023/day/{day}/input"
     cookies = dict(session=SESSION_ID)
     response = r.get(url, cookies=cookies)
-    return response.text
+
+    hasTerminalNewLine = response.text[-1] == "\n"
+    print(f"New Line: {hasTerminalNewLine}")
+    return response.text if not hasTerminalNewLine else response.text[:-1]
 
 
 def reduce(arr):
