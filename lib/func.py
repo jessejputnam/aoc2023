d254de44d5fa869b41117d559b991aa532cf4dea
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2023-12-08 14:39:18 -0500

    Solve day08 part 2

--- lib/func.py
+++ lib/func.py
@@ -1,4 +1,5 @@
 import requests as r
+import math
 from env import SESSION_ID
 
 
@@ -16,3 +17,31 @@ def reduce(arr):
     if len(arr) == 0:
         return 0
     return arr[0] + reduce(arr[1:])
+
+
+def getDivisors(num):
+    divs = []
+
+    sqrt = math.sqrt(num)
+    for i in range(math.floor(sqrt), 0, -1):
+        if num % i == 0:
+            divs.append(int(num / i))
+            if not i == sqrt:
+                divs.insert(0, i)
+    return divs
+
+
+def gcd(a, b):
+    a_div = getDivisors(a)
+    b_div = getDivisors(b)
+
+    gcd = 1
+    for num in a_div:
+        if (num in b_div) and num > gcd:
+            gcd = num
+
+    return gcd
+
+
+def lcm(a, b):
+    return int((a * b) / gcd(a, b))
